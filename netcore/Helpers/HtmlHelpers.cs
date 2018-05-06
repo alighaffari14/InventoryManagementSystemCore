0ef6aa2823a5f390301dd386bdf6266a672a7179
ismail hamzah <gohamzah@outlook.com>
2018-05-06 16:31:08 +0700

    adminlte demo

--- netcore/Helpers/HtmlHelpers.cs
+++ netcore/Helpers/HtmlHelpers.cs
@@ -16,14 +16,35 @@ namespace netcore
 
             string currentAction = (string)html.ViewContext.RouteData.Values["action"];
             string currentController = (string)html.ViewContext.RouteData.Values["controller"];
-
+            
             if (String.IsNullOrEmpty(controller))
                 controller = currentController;
 
             if (String.IsNullOrEmpty(action))
                 action = currentAction;
 
-            return controller == currentController && action == currentAction ?
+            bool result = (controller == currentController && action == currentAction);
+
+
+
+            return result ?
+                cssClass : String.Empty;
+        }
+
+        public static string IsSelected(this IHtmlHelper html, string actions = null)
+        {
+            var cssClass = "active";
+
+
+           string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+           string[] actionarr = !String.IsNullOrEmpty(actions) ? actions.Split(',') : new string[] { };
+
+
+            bool result = (actionarr.Contains<string>(currentAction));
+
+
+
+            return result ?
                 cssClass : String.Empty;
         }
 
--- netcore/MVC/PagesLTE.cs
+++ netcore/MVC/PagesLTE.cs
@@ -9,181 +9,181 @@ namespace lte.MVC
     {
         public static class Dashbaordv1
         {
-            public const string Url = "/Dashboard/Dashboardv1";
+            public const string Url = "/Adminlte/Dashboardv1";
         }
         public static class Dashbaordv2
         {
-            public const string Url = "/Dashboard/Dashboardv2";
+            public const string Url = "/Adminlte/Dashboardv2";
         }
 
         public static class LayoutTop
         {
-            public const string Url = "/Layout/Top";
+            public const string Url = "/Adminlte/Top";
         }
 
         public static class LayoutBoxed
         {
-            public const string Url = "/Layout/Boxed";
+            public const string Url = "/Adminlte/Boxed";
         }
 
         public static class LayoutFixed
         {
-            public const string Url = "/Layout/Fixed";
+            public const string Url = "/Adminlte/Fixed";
         }
 
         public static class LayoutCollapsed
         {
-            public const string Url = "/Layout/Collapsed";
+            public const string Url = "/Adminlte/Collapsed";
         }
 
         public static class WidgetIndex
         {
-            public const string Url = "/Widget/Index";
+            public const string Url = "/Adminlte/Widget";
         }
 
         public static class ChartChartJS
         {
-            public const string Url = "/Chart/ChartJS";
+            public const string Url = "/Adminlte/ChartJS";
         }
 
         public static class ChartMorris
         {
-            public const string Url = "/Chart/Morris";
+            public const string Url = "/Adminlte/Morris";
         }
 
         public static class ChartFlot
         {
-            public const string Url = "/Chart/Flot";
+            public const string Url = "/Adminlte/Flot";
         }
 
         public static class ChartInline
         {
-            public const string Url = "/Chart/Inline";
+            public const string Url = "/Adminlte/Inline";
         }
 
         public static class UIElementGeneral
         {
-            public const string Url = "/UIElement/General";
+            public const string Url = "/Adminlte/General";
         }
 
         public static class UIElementIcon
         {
-            public const string Url = "/UIElement/Icon";
+            public const string Url = "/Adminlte/Icon";
         }
 
         public static class UIElementButton
         {
-            public const string Url = "/UIElement/Button";
+            public const string Url = "/Adminlte/Button";
         }
 
         public static class UIElementSlider
         {
-            public const string Url = "/UIElement/Slider";
+            public const string Url = "/Adminlte/Slider";
         }
 
         public static class UIElementTimeline
         {
-            public const string Url = "/UIElement/Timeline";
+            public const string Url = "/Adminlte/Timeline";
         }
 
         public static class UIElementModal
         {
-            public const string Url = "/UIElement/Modal";
+            public const string Url = "/Adminlte/Modal";
         }
 
         public static class FormGeneral
         {
-            public const string Url = "/Form/General";
+            public const string Url = "/Adminlte/Form";
         }
 
         public static class FormAdvanced
         {
-            public const string Url = "/Form/Advanced";
+            public const string Url = "/Adminlte/Advanced";
         }
 
         public static class FormEditor
         {
-            public const string Url = "/Form/Editor";
+            public const string Url = "/Adminlte/Editor";
         }
 
         public static class TableSimple
         {
-            public const string Url = "/Table/Simple";
+            public const string Url = "/Adminlte/Simple";
         }
 
         public static class TableData
         {
-            public const string Url = "/Table/Data";
+            public const string Url = "/Adminlte/Data";
         }
 
         public static class CalendarIndex
         {
-            public const string Url = "/Calendar/Index";
+            public const string Url = "/Adminlte/Calendar";
         }
 
         public static class MailboxIndex
         {
-            public const string Url = "/Mailbox/Index";
+            public const string Url = "/Adminlte/Index";
         }
 
         public static class MailboxCompose
         {
-            public const string Url = "/Mailbox/Compose";
+            public const string Url = "/Adminlte/Compose";
         }
 
         public static class MailboxRead
         {
-            public const string Url = "/Mailbox/Read";
+            public const string Url = "/Adminlte/Read";
         }
 
         public static class ExampleInvoice
         {
-            public const string Url = "/Example/Invoice";
+            public const string Url = "/Adminlte/Invoice";
         }
 
         public static class ExampleInvoicePrint
         {
-            public const string Url = "/Example/InvoicePrint";
+            public const string Url = "/Adminlte/InvoicePrint";
         }
 
         public static class ExampleProfile
         {
-            public const string Url = "/Example/Profile";
+            public const string Url = "/Adminlte/Profile";
         }
 
         public static class ExampleLogin
         {
-            public const string Url = "/Example/Login";
+            public const string Url = "/Adminlte/Login";
         }
 
         public static class ExampleRegister
         {
-            public const string Url = "/Example/Register";
+            public const string Url = "/Adminlte/Register";
         }
 
         public static class ExampleLockscreen
         {
-            public const string Url = "/Example/Lockscreen";
+            public const string Url = "/Adminlte/Lockscreen";
         }
 
         public static class ExampleError404
         {
-            public const string Url = "/Example/Error404";
+            public const string Url = "/Adminlte/Error404";
         }
 
         public static class ExampleError500
         {
-            public const string Url = "/Example/Error500";
+            public const string Url = "/Adminlte/Error500";
         }
 
         public static class ExampleBlankPage
         {
-            public const string Url = "/Example/BlankPage";
+            public const string Url = "/Adminlte/BlankPage";
         }
 
         public static class ExamplePacePage
         {
-            public const string Url = "/Example/PacePage";
+            public const string Url = "/Adminlte/PacePage";
         }
     }
 }
