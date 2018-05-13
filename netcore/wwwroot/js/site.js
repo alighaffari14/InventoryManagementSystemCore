eef445b6ede0ff728884b50a55e6cd46f98f437f
ismail hamzah <gohamzah@outlook.com>
2018-05-13 16:31:06 +0700

    finishing auto select2 and date picker

--- netcore/wwwroot/js/site.js
+++ netcore/wwwroot/js/site.js
@@ -5,3 +5,11 @@
 function ShowMessageError(msg) {
     toastr.error(msg);
 }
+
+//Initialize Select2 Elements
+$('.select2').select2()
+
+//Date picker
+$('.datepicker').datepicker({
+    autoclose: true
+})
