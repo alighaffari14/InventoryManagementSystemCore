61b85a44f8bb69a35ef4e8f3e2344526f5b7286b
ismail hamzah <gohamzah@outlook.com>
2018-06-24 22:36:27 +0700

    done

--- netcore/Services/NetcoreService.cs
+++ netcore/Services/NetcoreService.cs
@@ -326,6 +326,134 @@ namespace netcore.Services
 
                 _context.Stage.AddRange(stages);
 
+                //create accountexecutive
+                List<AccountExecutive> aes = new List<AccountExecutive>()
+                {
+                    new AccountExecutive{accountExecutiveName = "Nancy Davolio", street1 = "507 - 20th Ave. E.  Apt. 2A"},
+                    new AccountExecutive{accountExecutiveName = "Andrew Fuller", street1 = "908 W. Capital Way"},
+                    new AccountExecutive{accountExecutiveName = "Janet Leverling", street1 = "722 Moss Bay Blvd."},
+                    new AccountExecutive{accountExecutiveName = "Margaret Peacock", street1 = "4110 Old Redmond Rd."},
+                    new AccountExecutive{accountExecutiveName = "Steven Buchanan", street1 = "14 Garrett Hill"},
+                    new AccountExecutive{accountExecutiveName = "Michael Suyama", street1 = "Coventry House  Miner Rd."},
+                    new AccountExecutive{accountExecutiveName = "Robert King", street1 = "Edgeham Hollow  Winchester Way"},
+                    new AccountExecutive{accountExecutiveName = "Laura Callahan", street1 = "4726 - 11th Ave. N.E."},
+                    new AccountExecutive{accountExecutiveName = "Anne Dodsworth", street1 = "7 Houndstooth Rd."}
+                };
+
+                _context.AccountExecutive.AddRange(aes);
+
+                //create lead
+                List<Lead> leads = new List<Lead>()
+                {
+                    new Lead{leadName = "Maria Anders", street1 = "Obere Str. 57", channel = channels[1]},
+                    new Lead{leadName = "Ana Trujillo", street1 = "Avda. de la Constitución 2222", channel = channels[1]},
+                    new Lead{leadName = "Antonio Moreno", street1 = "Mataderos  2312", channel = channels[1]},
+                    new Lead{leadName = "Thomas Hardy", street1 = "120 Hanover Sq.", channel = channels[1]},
+                    new Lead{leadName = "Christina Berglund", street1 = "Berguvsvägen  8", channel = channels[1]},
+                    new Lead{leadName = "Hanna Moos", street1 = "Forsterstr. 57", channel = channels[1]},
+                    new Lead{leadName = "Frédérique Citeaux", street1 = "24, place Kléber", channel = channels[1]},
+                    new Lead{leadName = "Martín Sommer", street1 = "C/ Araquil, 67", channel = channels[1]},
+                    new Lead{leadName = "Laurence Lebihan", street1 = "12, rue des Bouchers", channel = channels[1]},
+                    new Lead{leadName = "Elizabeth Lincoln", street1 = "23 Tsawassen Blvd.", channel = channels[1]},
+                    new Lead{leadName = "Victoria Ashworth", street1 = "Fauntleroy Circus", channel = channels[1]},
+                    new Lead{leadName = "Patricio Simpson", street1 = "Cerrito 333", channel = channels[1]},
+                    new Lead{leadName = "Francisco Chang", street1 = "Sierras de Granada 9993", channel = channels[1]},
+                    new Lead{leadName = "Yang Wang", street1 = "Hauptstr. 29", channel = channels[1]},
+                    new Lead{leadName = "Pedro Afonso", street1 = "Av. dos Lusíadas, 23", channel = channels[1]},
+                    new Lead{leadName = "Elizabeth Brown", street1 = "Berkeley Gardens 12  Brewery", channel = channels[1]},
+                    new Lead{leadName = "Sven Ottlieb", street1 = "Walserweg 21", channel = channels[1]},
+                    new Lead{leadName = "Janine Labrune", street1 = "67, rue des Cinquante Otages", channel = channels[1]},
+                    new Lead{leadName = "Ann Devon", street1 = "35 King George", channel = channels[1]},
+                    new Lead{leadName = "Roland Mendel", street1 = "Kirchgasse 6", channel = channels[1]},
+                };
+
+                _context.Lead.AddRange(leads);
+
+                Branch branch = new Branch() { branchName = "HQ", isDefaultBranch = true, street1 = "Rua Orós, 92" };
+                _context.Branch.Add(branch);
+
+                List<Warehouse> whs = new List<Warehouse>() {
+                    new Warehouse{warehouseName = "WH1", branch = branch, street1 = "Rua Orós, 92"},
+                    new Warehouse{warehouseName = "WH2", branch = branch, street1 = "C/ Moralzarzal, 86"},
+                    new Warehouse{warehouseName = "WH3", branch = branch, street1 = "184, chaussée de Tournai"},
+                    new Warehouse{warehouseName = "WH4", branch = branch, street1 = "Åkergatan 24"},
+                    new Warehouse{warehouseName = "WH5", branch = branch, street1 = "Berliner Platz 43"}
+                };
+
+                _context.Warehouse.AddRange(whs);
+
+                List<Product> products = new List<Product>() {
+                    new Product{productCode = "Chai", productName = "Chai", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Chang", productName = "Chang", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Aniseed Syrup", productName = "Aniseed Syrup", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Chef Anton's Cajun Seasoning", productName = "Chef Anton's Cajun Seasoning", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Chef Anton's Gumbo Mix", productName = "Chef Anton's Gumbo Mix", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Grandma's Boysenberry Spread", productName = "Grandma's Boysenberry Spread", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Uncle Bob's Organic Dried Pears", productName = "Uncle Bob's Organic Dried Pears", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Northwoods Cranberry Sauce", productName = "Northwoods Cranberry Sauce", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Mishi Kobe Niku", productName = "Mishi Kobe Niku", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Ikura", productName = "Ikura", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Queso Cabrales", productName = "Queso Cabrales", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Queso Manchego La Pastora", productName = "Queso Manchego La Pastora", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Konbu", productName = "Konbu", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Tofu", productName = "Tofu", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Genen Shouyu", productName = "Genen Shouyu", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Pavlova", productName = "Pavlova", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Alice Mutton", productName = "Alice Mutton", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Carnarvon Tigers", productName = "Carnarvon Tigers", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Teatime Chocolate Biscuits", productName = "Teatime Chocolate Biscuits", productType = ProductType.Food, uom = UOM.Pcs},
+                    new Product{productCode = "Sir Rodney's Marmalade", productName = "Sir Rodney's Marmalade", productType = ProductType.Food, uom = UOM.Pcs}
+
+                };
+                _context.Product.AddRange(products);
+
+                List<Vendor> vendors = new List<Vendor>() {
+                    new Vendor{vendorName = "Exotic Liquids", street1 = "49 Gilbert St.", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "New Orleans Cajun Delights", street1 = "P.O. Box 78934", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Grandma Kelly's Homestead", street1 = "707 Oxford Rd.", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Tokyo Traders", street1 = "9-8 Sekimai Musashino-shi", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Cooperativa de Quesos 'Las Cabras'", street1 = "Calle del Rosal 4", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Mayumi's", street1 = "92 Setsuko Chuo-ku", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Pavlova, Ltd.", street1 = "74 Rose St. Moonie Ponds", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Specialty Biscuits, Ltd.", street1 = "29 King's Way", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "PB Knäckebröd AB", street1 = "Kaloadagatan 13", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Refrescos Americanas LTDA", street1 = "Av. das Americanas 12.890", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Heli Süßwaren GmbH & Co. KG", street1 = "Tiergartenstraße 5", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Plutzer Lebensmittelgroßmärkte AG", street1 = "Bogenallee 51", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Nord-Ost-Fisch Handelsgesellschaft mbH", street1 = "Frahmredder 112a", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Formaggi Fortini s.r.l.", street1 = "Viale Dante, 75", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Norske Meierier", street1 = "Hatlevegen 5", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Bigfoot Breweries", street1 = "3400 - 8th Avenue Suite 210", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Svensk Sjöföda AB", street1 = "Brovallavägen 231", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "Aux joyeux ecclésiastiques", street1 = "203, Rue des Francs-Bourgeois", size = BusinessSize.Enterprise},
+                    new Vendor{vendorName = "New England Seafood Cannery", street1 = "Order Processing Dept. 2100 Paul Revere Blvd.", size = BusinessSize.Enterprise}
+                };
+                _context.Vendor.AddRange(vendors);
+
+                List<Customer> customers = new List<Customer>() {
+                    new Customer{customerName = "Hanari Carnes", street1 = "Rua do Paço, 67", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "HILARION-Abastos", street1 = "Carrera 22 con Ave. Carlos Soublette #8-35", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Hungry Coyote Import Store", street1 = "City Center Plaza 516 Main St.", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Hungry Owl All-Night Grocers", street1 = "8 Johnstown Road", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Island Trading", street1 = "Garden House Crowther Way", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Königlich Essen", street1 = "Maubelstr. 90", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "La corne d'abondance", street1 = "67, avenue de l'Europe", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "La maison d'Asie", street1 = "1 rue Alsace-Lorraine", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Laughing Bacchus Wine Cellars", street1 = "1900 Oak St.", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Lazy K Kountry Store", street1 = "12 Orchestra Terrace", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Lehmanns Marktstand", street1 = "Magazinweg 7", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Let's Stop N Shop", street1 = "87 Polk St. Suite 5", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "LILA-Supermercado", street1 = "Carrera 52 con Ave. Bolívar #65-98 Llano Largo", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "LINO-Delicateses", street1 = "Ave. 5 de Mayo Porlamar", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Lonesome Pine Restaurant", street1 = "89 Chiaroscuro Rd.", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Magazzini Alimentari Riuniti", street1 = "Via Ludovico il Moro 22", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Maison Dewey", street1 = "Rue Joseph-Bens 532", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Mère Paillarde", street1 = "43 rue St. Laurent", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Morgenstern Gesundkost", street1 = "Heerstr. 22", size = BusinessSize.Enterprise},
+                    new Customer{customerName = "Old World Delicatessen", street1 = "2743 Bering St.", size = BusinessSize.Enterprise}
+                };
+                _context.Customer.AddRange(customers);
+
                 await _context.SaveChangesAsync();
             }
             catch (Exception)
